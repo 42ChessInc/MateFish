d68711b060c4c2a2d533c9d5f6c8a128c4e3d3fa
Rafael Luis Yanase de Rezende <rafael.yanase@gmail.com>
2025-07-31 13:07:36 +0100

    Initial commit

---
+++ Makefile
@@ -0,0 +1,11 @@
+CC = cc
+
+CFLAGS = -Wall -Werror -Wextra -Iinclude
+
+SRCS_DIR = srcs
+
+OBJS_DIR = objs
+
+INC_DIR = include
+
+
---
+++ include/board.h
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   board.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rluis-ya <rluis-ya@student.42porto.com>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/07/31 12:29:39 by rluis-ya          #+#    #+#             */
+/*   Updated: 2025/07/31 12:43:38 by rluis-ya         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef BOARD_H
+#define BOARD_H
+
+typedef uint64_t	Bitboard;
+
+typedef struct
+{
+	Bitboard	pieces[2][6]; //Squares occupied by each piece (6) for each color (2)
+
+	Bitboard	ocuppied; //All ocuppied squares (both black and white)
+	Bitboard	ocuppiedColor[2]; //Ocuppied squares by color
+}	t_board;
+
+#endif
